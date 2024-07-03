a0f4e9b44554c0eab6a3fed2d226a135fc341c10
yujin4sth00 <yujin4sth00@gmail.com>
2024-07-03 10:09:49 +0900

    first test

---
+++ dockerImageTest/src/main/java/com/ohgiraffers/dockertest/main/MainController.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.dockertest.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/main")
+    public String defaultLocation(){
+        return "main/main";
+    }
+}
